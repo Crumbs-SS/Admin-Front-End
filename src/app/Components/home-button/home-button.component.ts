abe0a47e305dbefc9d1b1a06ef1830d253ba19f8
elijahcbrooks <elijahcbrooks@gmail.com>
2021-10-25 04:27:22 -0500

    crum-179 feat: refactoring for home button

--- src/app/Components/home-button/home-button.component.ts
+++ src/app/Components/home-button/home-button.component.ts
@@ -1,26 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart, Event  } from '@angular/router';
+import { Router, NavigationStart, Event } from '@angular/router';
 
 @Component({
   selector: 'app-home-button',
   templateUrl: './home-button.component.html',
   styleUrls: ['./home-button.component.css']
 })
-export class HomeButtonComponent implements OnInit {
+export class HomeButtonComponent {
 
   currentRoute: String = '';
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
-    console.log(this.router.events);
-    this.router.events.subscribe((event: Event) => {
-      if(event instanceof NavigationStart) {
-        this.currentRoute = event.url
-      }
-    });
-  }
 
-  redirectToHome(){
-    this.router.navigateByUrl('/crumbs/admin')
+  hasRoute(route: string) {
+    return route === this.router.url;
   }
 }
